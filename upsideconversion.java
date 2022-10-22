a3dea206f4eee6937ad409dca8d99fc7f0f7484b
Aman Raj <78196771+01amanraj@users.noreply.github.com>
2022-10-22 15:00:21 +0530

    Add files via upload

---
+++ upsideconversion.java
@@ -0,0 +1,14 @@
+package day2.src;
+
+public class upsideconversion {
+    public static void main(String[] args) {
+        double a = 29d;
+        System.out.println("double representation"+ a);
+        float b = (float)a;
+        System.out.println("float representation"+ b);
+        long c = (long)a;
+        System.out.println("long representation"+ c);
+        int d= (int)a;
+        System.out.println("int representation"+ d);
+    }
+}
